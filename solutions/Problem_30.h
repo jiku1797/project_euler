31015a262281a182fe1879c96adeca8fb78cab61
jiku1797 <jiku1797@gmail.com>
2024-08-10 11:05:33 +0200

    improve solution description for problem 30

--- solutions/Problem_30.h
+++ solutions/Problem_30.h
@@ -9,13 +9,15 @@
 
 /*
 * Digit Fifth Powers
-* Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+* Surprisingly there are only three numbers that can be written as the sum of
+* fourth powers of their digits:
 * 1634 = 1^4 + 6^4 + 3^4 + 4^4
 * 8208 = 8^4 + 2^4 + 0^4 + 8^4
 * 9474 = 9^4 + 4^4 + 7^4 + 4^4
 * As 1 = 1^4 is not a sum it is not included.
 * The sum of these numbers is 1634 + 8208 + 9474 = 19316.
-* Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+* Find the sum of all the numbers that can be written as the sum of fifth powers
+* of their digits.
 */
 
 namespace problem_30
@@ -26,9 +28,9 @@ inline underlying_t problem_30()
 {
   static constexpr underlying_t exponent = 5;
 
-  // there can't be a number with 7 digits (or more) which fulfils the condition for exponent=6
-  // since if all digits are 9, then
-  // 7 digits: 7 * 9^5 = 413343
+  // There can't be a number with 7 digits (or more) which fulfils the condition
+  // for exponent equal to 5 since if all digits are 9, then
+  // sum of 5:th powers of digits = 7 * 9^5 = 413343 is only 6 digits long
   static constexpr const underlying_t upper_bound = 6 * 9 * 9 * 9 * 9 * 9;
 
   underlying_t sum = 0;
